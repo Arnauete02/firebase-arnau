080f657e860d28d6be5080aa5bd36a8f626605c6
Arnauete02 <agraciataberner02@gmail.com>
2022-01-31 23:46:12 +0100

    .

--- app/src/main/java/com/example/firebasetemplate/SignInFragment.java
+++ app/src/main/java/com/example/firebasetemplate/SignInFragment.java
@@ -1,14 +1,22 @@
 package com.example.firebasetemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.firebasetemplate.databinding.FragmentSignInBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class SignInFragment extends AppFragment {
@@ -27,6 +35,7 @@ public class SignInFragment extends AppFragment {
 
         binding.googleSignIn.setOnClickListener(view1 -> {
             navController.navigate(R.id.action_signInFragment_to_postsHomeFragment);
+            signInClient.launch(GoogleSignIn.getClient(this, new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).build()).getSignInIntent());
         });
 
         binding.emailSignIn.setOnClickListener(view1 -> {
@@ -37,4 +46,10 @@ public class SignInFragment extends AppFragment {
             navController.navigate(R.id.action_signInFragment_to_registerFragment);
         });
     }
+
+    ActivityResultLauncher<Intent> signInClient = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        try {
+            FirebaseAuth.getInstance().signInWithCredential(GoogleAuthProvider.getCredential(GoogleSignIn.getSignedInAccountFromIntent(result.getData()).getResult(ApiException.class).getIdToken(), null));
+        } catch (ApiException e) {}
+    });
 }
